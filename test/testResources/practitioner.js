79c25511eb5b1c43cb3b85d3297af376c044c6e8
Parvati Menon <parumenon.pm@gmail.com>
2018-06-11 20:01:53 -0400

    Issue #19: Created fhir-human-certification This element has select option of type of qualification of healthcare provider.It has has two fields to iput system identifier and identifier value of qualification. It has GET properties from URL and JSON and takes input from user too.

---
+++ test/testResources/practitioner.js
@@ -0,0 +1,48 @@
+export default{
+    "resourceType": "Practitioner",
+    "id": "4146083",
+    "meta": {
+    "versionId": "1",
+        "lastUpdated": "2018-06-06T16:55:35.637+00:00"
+},
+    "identifier": [
+    {
+        "use": "official",
+        "system": "urn:oid:2.16.840.1.113883.2.9.2.50.4.2",
+        "value": "007480"
+    },
+    {
+        "use": "official",
+        "system": "urn:oid:2.16.840.1.113883.2.9.4.3.2",
+        "value": "PRNBDN67R28L736C"
+    }
+],
+    "name": [
+    {
+        "text": "Prandin Oberdan",
+        "family": "Prandin",
+        "given": [
+            "Oberdan"
+        ]
+    }
+],
+    "qualification": [
+    {
+        "identifier": [
+            {
+                "system": "urn:oid:2.16.840.1.113883.2.9.2.50503.4.15",
+                "value": "301_EX113"
+            }
+        ],
+        "code": {
+            "coding": [
+                {
+                    "system": "urn:oid:2.16.840.1.113883.2.9.5.1.111",
+                    "code": "MMG",
+                    "display": "Medico di Medicina Generale"
+                }
+            ]
+        }
+    }
+]
+}
