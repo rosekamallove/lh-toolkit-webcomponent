9278ece8bc0709b3746d38173d453596fb92c252
paru M <parumenon.pm@gmail.com>
2018-08-07 13:32:37 -0400

    Issue #73: Created fhir-searchbox Create a component that uses vaadin-combo-box and mwc-button to search patient/practitioner/organization/location names as per the URL provided and redirect to the page of the selection. It shows name of the search with its identifier. Unit test is marked as "to do" as vaadin-combo-box could not use auto-type to retrieve values.

---
+++ packages/fhir-searchbox/fhir-searchbox.js
@@ -0,0 +1,100 @@
+/**
+ @license
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/**
+ * `<fhir-allergy-code>` adds code of allergy to your page. Uses combo-box and iron-ajax
+ * In typical use, just use `<fhir-allergy-code url=""></fhir-allergy-code>`
+ * @customElement
+ * @polymer
+ * @demo https://librehealth.gitlab.io/toolkit/lh-toolkit-webcomponents/demos/fhir-allergy-code.html
+ *
+ */
+
+import {LitElement, html} from '@polymer/lit-element/lit-element.js';
+import '@material/mwc-textfield/mwc-textfield.js'
+import '@material/mwc-button/mwc-button.js';
+import '@polymer/iron-ajax/iron-ajax.js';
+import '@vaadin/vaadin-combo-box/vaadin-combo-box.js';
+import '@vaadin/vaadin-combo-box/theme/lumo/vaadin-combo-box-light.js';
+
+
+class FhirSearchbox extends LitElement {
+    static get properties() {
+        return {
+            /**url is used to make AJAX call to FHIR resource. Default: null */
+            url: String,
+            /**value is used to take the input value of each field*/
+            value: Object,
+            /**relocateurl is used to give url that is used for redirection on click of the button*/
+            relocateurl: String,
+            /**resourceType is used to determine which type of resource is being searched*/
+            resourceType: String
+        }
+    }
+
+    constructor() {
+        super();
+        this.resourceType ="";
+    }
+
+    _didRender() {
+		FhirSearchbox.relocateurl = this.relocateurl;
+        this.shadowRoot.getElementById('ajax').addEventListener('iron-ajax-response', function (e) {
+            var items = [];
+            var box = e.target.parentNode.querySelector('#search');
+           // var entry = e.detail.response.entry;
+            for (let entry of e.detail.response.entry) {
+                var type = entry.resource.resourceType;
+                FhirSearchbox.resourceType = type;
+                if (type == "Patient" || type == "Practitioner") {
+                    items.push({
+                        label: entry.resource.name[0].given[0] + ' ' + entry.resource.name[0].family + ", id:" + entry.resource.id,
+                        value: entry.resource.id
+                    });
+                }
+                else if (type == "Organization" || type == "Location") {
+                    items.push({
+                        label: entry.resource.name+ ", id:" + entry.resource.id,
+                        value: entry.resource.id
+                    })
+                }
+            }
+            console
+            box.items = items;
+           console.log(items);
+        });
+        this.shadowRoot.getElementById('button').addEventListener('click', function (e) {
+            var val = e.target.parentNode.querySelector('#search').value;
+          window.location.href = FhirSearchbox.relocateurl + '?'+FhirSearchbox.resourceType+'Id=' + val;
+        });
+    }
+
+
+    _render({url, relocateurl}) {
+        return html`
+    <style>
+    vaadin-combo-box {
+      width: 500px;
+    }
+     </style>
+     <vaadin-combo-box id="search" label=" Search" placeholder="Type.." on-filter-changed="${e => this.makeQuery(e.detail.value)}">
+     <template>[[item.label]]</template>
+     </vaadin-combo-box>
+     <mwc-button id="button" raised>Go</mwc-button>
+     <iron-ajax bubbles id="ajax" handle-as="json" url="${url}"></iron-ajax>
+`;
+    }
+
+    makeQuery(param) {
+        if (param.length > 2) {
+            this.shadowRoot.getElementById('ajax').params = {"name": param};
+            this.shadowRoot.getElementById('ajax').generateRequest()
+        }
+    }
+
+}
+
+window.customElements.define('fhir-searchbox', FhirSearchbox);
