59db707d6a03e8315129112d3ed07f5ac38f8d19
Rose Kamal Love <rosekamallove@gmail.com>
2022-09-08 02:02:49 +0530

    added: fhir-radiology-filter: accesionNumber

--- packages/fhir-radiology-filter/fhir-radiology-filter.js
+++ packages/fhir-radiology-filter/fhir-radiology-filter.js
@@ -68,92 +68,20 @@ class FhirRadiologyFilter extends LitElement {
     if (typeof this.value == "string") {
       this.value = JSON.parse(this.value);
     }
-    return html`${this.value.map(
-        (i, index) => html`
-          <div id="humanNameDiv">
-            <mwc-formfield label="Name:" alignEnd>
-              ${this.useField !== "false"
-                ? html`
-                    <mwc-select
-                      label="Use"
-                      outlined
-                      class="useField"
-                      .value="${i.use}"
-                      @change="${(e) =>
-                        (this.value[index].use = e.target.value)}"
-                    >
-                      <mwc-list-item value="usual">Usual</mwc-list-item>
-                      <mwc-list-item value="official">Official</mwc-list-item>
-                      <mwc-list-item value="temp">Temporary</mwc-list-item>
-                      <mwc-list-item value="nickname">Nickname</mwc-list-item>
-                      <mwc-list-item value="anonymous">Anonymous</mwc-list-item>
-                      <mwc-list-item value="old">Old</mwc-list-item>
-                      <mwc-list-item value="maiden">Maiden</mwc-list-item>
-                    </mwc-select>
-                  `
-                : ""}
-              ${this.prefixField !== "false"
-                ? html`<mwc-textfield
-                    outlined
-                    class="prefixField"
-                    .value="${i.prefix || ""}"
-                    @input="${(e) =>
-                      (this.value[index].prefix = e.target.value)}"
-                    id="prefix"
-                    label="Prefix"
-                  ></mwc-textfield>`
-                : ""}
-              ${this.fName !== "false"
-                ? html`<mwc-textfield
-                    outlined
-                    class="fName"
-                    .value="${this.mName !== "false"
-                      ? i.given[0]
-                      : i.given || ""}"
-                    @input="${(e) =>
-                      this.mName !== "false"
-                        ? (this.value[index].given[0] = e.target.value)
-                        : (this.value[index].given = e.target.value)}"
-                    label="First Name:"
-                  ></mwc-textfield>`
-                : ""}
-              ${this.mName !== "false"
-                ? html`<mwc-textfield
-                    outlined
-                    class="mName"
-                    .value="${i.given[1]}"
-                    @input="${(e) =>
-                      (this.value[index].given[1] = e.target.value)}"
-                    label="Middle Name:"
-                  ></mwc-textfield>`
-                : ""}
-              ${this.lName !== "false"
-                ? html`<mwc-textfield
-                    outlined
-                    class="lName"
-                    .value="${i.family || ""}"
-                    @input="${(e) =>
-                      (this.value[index].family = e.target.value)}"
-                    label="Last Name:"
-                  ></mwc-textfield>`
-                : ""}
-              ${this.periodField !== "false"
-                ? html`<fhir-period class="periodField"></fhir-period>`
-                : ""}
-              ${this.suffixField !== "false"
-                ? html`<mwc-textfield
-                    outlined
-                    class="suffixField"
-                    value="${i.suffix || ""}"
-                    @input="${(e) =>
-                      (this.value[index].suffix = e.target.value)}"
-                    label="Suffix"
-                  ></mwc-textfield>`
-                : ""}
-            </mwc-formfield>
-          </div>
-        `
-      )}
+    return html` <div className="radiolgyFilterDiv">
+        ${this.accessionNumber !== "false"
+          ? html`<mwc-textfield
+              outlined
+              class="accessionNumber"
+              @input="${(e) =>
+                (this.value[index].accessionNumber = e.target.value)}"
+              id="accessionNumber"
+              label="Accession Number"
+              <mwc-textfield
+            />`
+          : ""}
+      </div>
+
       <iron-ajax
         id="ajax"
         bubbles
