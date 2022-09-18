25e7bf86ef4ac9ac30f43feb7eefefda68f270ab
Rose Kamal Love <rosekamallove@gmail.com>
2022-09-18 21:17:25 +0530

    populated: table radiology orders

--- packages/fhir-radiology-list/fhir-radiology-list.js
+++ packages/fhir-radiology-list/fhir-radiology-list.js
@@ -14,103 +14,80 @@
  *
  */
 
-import {LitElement, html} from 'lit-element';
-import '@material/mwc-textfield/mwc-textfield.js'
-import '@polymer/iron-ajax/iron-ajax.js';
-import '@vaadin/vaadin-grid/vaadin-grid.js';
-import '@vaadin/vaadin-grid/vaadin-grid-selection-column.js';
+import { LitElement, html } from "lit-element";
+import "@material/mwc-textfield/mwc-textfield.js";
+import "@polymer/iron-ajax/iron-ajax.js";
+import "@vaadin/vaadin-grid/vaadin-grid.js";
+import "@vaadin/vaadin-grid/vaadin-grid-selection-column.js";
+import { exampleRadiologyList } from "./example_data";
 
 class FhirRadiologyList extends LitElement {
-    static undefinedToBlank(obj) {
-        if (obj == undefined) {
-            return '';
-        }
-        else {
-            return obj;
-        }
+  static undefinedToBlank(obj) {
+    if (obj == undefined) {
+      return "";
+    } else {
+      return obj;
     }
+  }
 
-    static get properties() {
-        return {
-            /**url is used to make AJAX call to FHIR resource. Default: null */
-            url: {type: String},
-            /**value is used to take the input value of field*/
-            value: {type: Object},
-            /**relocateurl is used to give url that is used for redirection on click of the button*/
-            relocateurl: {type: String},
-            /**resourceType is used to determine which type of resource is being searched*/
-            resourceType: {type: String}
-        }
-    }
+  static get properties() {
+    return {
+      /**url is used to make AJAX call to FHIR resource. Default: null */
+      url: { type: String },
+      /**orders is used to take the Array of Orders*/
+      orders: { type: Array },
+    };
+  }
 
-    constructor() {
-        super();
-        this.value = {};
-        this.resourceType ="";
-    }
+  constructor() {
+    super();
+    this.orders = exampleRadiologyList;
+  }
 
-    updated() {
-        FhirSearch.relocateurl = this.relocateurl;
-        this.shadowRoot.getElementById('ajax').addEventListener('iron-ajax-response', function (e) {
-            var grid = e.target.parentNode.querySelector('#table');
-            var items = [];
-            for (let entry of e.detail.response.entry) {
-                var type = entry.resource.resourceType;
-                FhirSearch.resourceType = type;
-                //Checks search for human names
-                if (type == "Patient" || type == "Practitioner") {
-                    items.push({
-                        id: entry.resource.id,
-                        name: entry.resource.name[0].family + " " + entry.resource.name[0].given,
-                        active: FhirSearch.undefinedToBlank(entry.resource.active)
-                    });
-                }
-                //checks search for non-human names
-                else if (type == "Organization" || type == "Location") {
-                    items.push({
-                        id: entry.resource.id,
-                        name: entry.resource.name,
-                        active: FhirSearch.undefinedToBlank(entry.resource.active)
-                    });
-                }
-            }
-            grid.items = items;
-        });
-        //Event Listener for active-item-changed in the grid and click used to redirect to the desired location.
-        this.shadowRoot.getElementById('table').addEventListener('active-item-changed', function (e) {
-            window.location.href = FhirSearch.relocateurl + '?' + FhirSearch.resourceType + 'Id=' + e.detail.value.id;
-        });
-    }
+  updated() {
+    console.log(this.orders);
+    const grid = this.shadowRoot.getElementById("table");
+    grid.items = this.orders;
+  }
 
-    render() {
-        return html`
-      <mwc-textfield outlined id="searchField" @input="${e => this.makeQuery(e.target.value)}" label="Search"></mwc-textfield>
-      <iron-ajax bubbles id="ajax" handle-as="json" .url="${this.url}"></iron-ajax>
-      <vaadin-grid aria-label="Names" id="table">
-         <vaadin-grid-selection-column auto-select hidden></vaadin-grid-selection-column>
-         <vaadin-grid-column width="50px " flex="0 ">
-            <template class="header ">Identifier</template>
-            <template>[[item.id]]</></template>
+  render() {
+    return html`
+      <vaadin-grid aria-label="Names" id="table" style="font-size: 80%;">
+         <vaadin-grid-column width="0px" flex="0 ">
+            <template class="header ">Accession Number</template>
+            <template>[[item.accessionNumber]]</></template>
          </vaadin-grid-column>
          <vaadin-grid-column>
-            <template class="header ">Name</template>
-            <template>[[item.name]]</template>
+            <template class="header ">Patient</template>
+            <template>[[item.patient.display]]</template>
          </vaadin-grid-column>
          <vaadin-grid-column>
-            <template class="header">Active Status</template>
-            <template>[[item.active]]</template>
+            <template class="header">Urgency</template>
+            <template>[[item.urgency]]</template>
+         </vaadin-grid-column>
+         <vaadin-grid-column>
+            <template class="header">Imagin Procedure</template>
+            <template>[[item.concept.display]]</template>
+         </vaadin-grid-column>
+         <vaadin-grid-column>
+            <template class="header">Referring Physician</template>
+            <template>[[item.orderer.display]]</template>
+         </vaadin-grid-column>
+         <vaadin-grid-column>
+            <template class="header">Scheduled Date</template>
+            <template>[[item.scheduledDate]]</template>
+         </vaadin-grid-column>
+         <vaadin-grid-column>
+            <template class="header">Date Activated</template>
+            <template>[[item.dateActivated]]</template>
+         </vaadin-grid-column>
+         <vaadin-grid-column>
+            <template class="header">Date Stopped</template>
+            <template>[[item.dateActivated]]</template>
          </vaadin-grid-column>
       </vaadin-grid>
-`;
-    }
-//Used to make query when the user types two or more characters
-    makeQuery(param) {
-        if (param.length > 2) {
-            this.shadowRoot.getElementById('ajax').params = {"name": param};
-            this.shadowRoot.getElementById('ajax').generateRequest()
-        }
-    }
-
+    `;
+  }
 }
 
-window.customElements.define('fhir-radiology-list', FhirRadiologyList);
+window.customElements.define("fhir-radiology-list", FhirRadiologyList);
